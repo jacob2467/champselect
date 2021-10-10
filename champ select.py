68e4739f5001a9a52633b5d2cc538e6ace22c0de
jacob2467 <92223080+jacob2467@users.noreply.github.com>
2021-10-10 18:17:04 -0400

    Add files via upload

--- champ select.py
+++ champ select.py
@@ -1,13 +1,26 @@
 import pyautogui
 import time
+from PIL import Image, ImageGrab
+import cv2
+import numpy as nm
+import pytesseract
 
 pyautogui.FAILSAFE = True
-time.sleep(5)
+time.sleep(2)
 
 
+def imToString():
+    # Path of tesseract executable
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
+    image1 = ImageGrab.grab(bbox=(160, 200, 2724, 1600))
+    string = pytesseract.image_to_string(cv2.cvtColor(nm.array(image1), cv2.COLOR_BGR2GRAY), lang='eng')
+    return string
+
+
+# Choose pick/ban
 playing = str(input("Who would you like to play?\n"))
 banning = str(input("Who would you like to ban?\n"))
-time.sleep(1)
+time.sleep(2)
 
 # Will differ depending on resolution. This works for my Mac that has a 2880x1800 Display.
 # Also, you obviously have to keep the window in the same place every time
@@ -20,18 +33,18 @@ lockiny = 700
 
 
 def lockin():
-    pyautogui.moveTo(lockinx, lockiny, duration=1)
+    pyautogui.moveTo(lockinx, lockiny, duration=0)
     pyautogui.click()
 
 
 def character():
-    pyautogui.moveTo(characterx, charactery, duration=1)
+    pyautogui.moveTo(characterx, charactery, duration=0)
     pyautogui.click()
     lockin()
 
 
 def searchplay():
-    pyautogui.moveTo(searchx, searchy, duration=1)
+    pyautogui.moveTo(searchx, searchy, duration=0)
     time.sleep(.5)
     pyautogui.click()
     pyautogui.hotkey('command' 'a')
@@ -42,7 +55,7 @@ def searchplay():
 
 
 def searchban():
-    pyautogui.moveTo(searchx, searchy, duration=1)
+    pyautogui.moveTo(searchx, searchy, duration=0)
     time.sleep(.5)
     pyautogui.click()
     pyautogui.hotkey('command' 'a')
@@ -52,11 +65,31 @@ def searchban():
     character()
 
 
-searchplay()
-time.sleep(15)
-searchban()
-time.sleep(30)
-for i in range(10):
-    searchplay()
-    time.sleep(10)
-print("Done!")
+declare = "declare"
+ban = "ban"
+choose = "choose"
+loadout = "loadout"
+
+hasdeclared = False
+while hasdeclared == False:
+    imToString()
+    string = imToString()
+    time.sleep(1)
+    if declare in string.lower():
+        searchplay()
+        hasdeclared = True
+    elif choose in string.lower():
+        searchplay()
+        exit()
+    else:
+        imToString()
+while hasdeclared == True:
+    imToString()
+    string = imToString()
+    if ban in string.lower():
+        searchban()
+    elif choose in string.lower():
+        searchplay()
+    elif loadout in string.lower():
+        print("Done!")
+        exit()
