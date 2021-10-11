d7c63781973d84ec4b460bbd62d845b5f2eb377a
jacob2467 <92223080+jacob2467@users.noreply.github.com>
2021-10-11 15:46:27 -0400

    Add files via upload

--- champ select.py
+++ champ select.py
@@ -6,48 +6,49 @@ import numpy as nm
 import pytesseract
 
 pyautogui.FAILSAFE = True
-time.sleep(2)
 
 
 def imToString():
     # Path of tesseract executable
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\jamo0\AppData\Local\Programs\Python\Python39\Scripts\pytesseract.exe'
-    image1 = ImageGrab.grab
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+    image1 = ImageGrab.grab(bbox=(320,180,1600,825))
     string = pytesseract.image_to_string(cv2.cvtColor(nm.array(image1), cv2.COLOR_BGR2GRAY), lang='eng')
+    print(string)
     return string
 
 
 # Choose pick/ban
-playing = "Miss Fortune" #str(input("Who would you like to play?\n"))
-banning = "Morgana" #str(input("Who would you like to ban?\n"))
-time.sleep(2)
+playing = str(input("Who would you like to play?\n"))
+banning = str(input("Who would you like to ban?\n"))
 
-# Will differ depending on resolution. This works for my Mac that has a 2880x1800 Display.
-# Also, you obviously have to keep the window in the same place every time
+# Will differ depending on resolution. Also, you obviously have to keep the window in the same place every time
 searchx = 1150
-searchy = 340
+searchy = 265
 characterx = 700
-charactery = 375
+charactery = 325
 lockinx = 950
-lockiny = 830
+lockiny = 775
 
 
 def lockin():
-    pyautogui.moveTo(lockinx, lockiny, duration=0)
+    pyautogui.moveTo(lockinx, lockiny, duration=0.1)
+    pyautogui.click()
     pyautogui.click()
 
 
 def character():
-    pyautogui.moveTo(characterx, charactery, duration=0)
+    pyautogui.moveTo(characterx, charactery, duration=0.1)
     pyautogui.click()
+    pyautogui.click()
+    time.sleep(0.1)
     lockin()
 
 
 def searchplay():
-    pyautogui.moveTo(searchx, searchy, duration=0)
+    pyautogui.moveTo(searchx, searchy, duration=0.1)
     time.sleep(.5)
     pyautogui.click()
-    pyautogui.hotkey('command' 'a')
+    pyautogui.hotkey('ctrl' 'a')
     pyautogui.press('backspace')
     pyautogui.typewrite(playing)
     pyautogui.press('enter')
@@ -55,10 +56,10 @@ def searchplay():
 
 
 def searchban():
-    pyautogui.moveTo(searchx, searchy, duration=0)
+    pyautogui.moveTo(searchx, searchy, duration=0.1)
     time.sleep(.5)
     pyautogui.click()
-    pyautogui.hotkey('command' 'a')
+    pyautogui.hotkey('ctrl' 'a')
     pyautogui.press('backspace')
     pyautogui.typewrite(banning)
     pyautogui.press('enter')
@@ -71,6 +72,7 @@ choose = "choose"
 loadout = "loadout"
 
 hasdeclared = False
+hasbanned = False
 while hasdeclared == False:
     imToString()
     string = imToString()
@@ -84,12 +86,15 @@ while hasdeclared == False:
     else:
         imToString()
 while hasdeclared == True:
-    imToString()
-    string = imToString()
-    if ban in string.lower():
-        searchban()
-    elif choose in string.lower():
-        searchplay()
-    elif loadout in string.lower():
-        print("Done!")
-        exit()
+    while hasbanned == False:
+        imToString()
+        string = imToString()
+        if ban in string.lower():
+            searchban()
+            hasbanned = True
+        elif choose in string.lower():
+            searchplay()
+            exit()
+        elif loadout in string.lower():
+            print("Done!")
+            exit()
